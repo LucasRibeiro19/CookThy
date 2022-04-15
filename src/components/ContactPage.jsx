ba7e2db7a8bc19700eff143b2933eca6b8bf3e47
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-04-15 23:53:02 +0200

    Contact Update

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import Header from './Header';
+import { Typography } from '@mui/material';
 
 function ContactPage() {
+
+
   return (
     <div>
       <Header/>
-      ContactPage
+      <div style={{margin:'1em'}}>
+        <Typography
+          variant="h2"
+          fontFamily="Poppins"
+          color='#01937C'
+          fontWeight='bold'
+        >
+          Contact us !
+        </Typography>
+      </div>
     </div>
   )
 }
