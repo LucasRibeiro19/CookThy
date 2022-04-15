d24f07095bec6b1e7094f86d3609b3721175dcb2
claireroman <claire.roman.08@orange.fr>
2022-04-15 22:09:25 +0200

    Formular to add a recipe

--- src/components/ContactPage.jsx
+++ src/components/ContactPage.jsx
@@ -1,13 +1,69 @@
-import React from 'react';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 import Header from './Header';
+import { Typography } from '@mui/material';
 
-function ContactPage() {
+export default function MultilineTextFields() {
+  const [value, setValue] = React.useState('Controlled');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
   return (
-    <div>
-      <Header/>
-      ContactPage
-    </div>
-  )
-}
+    <>
+    <Header/>
 
-export default ContactPage
+
+    <Typography
+                    variant="h2"
+                    fontFamily="Poppins"
+                    color='#01937C'
+                    component="div"
+                    sx={{  display: {fontWeight:'bold'}, padding:"5%" }} 
+                > Please add a recipe ! 
+            </Typography>
+
+
+    <Box
+      component="form"
+      sx={{
+        '& .MuiTextField-root': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+      <div>
+        <TextField
+          id="outlined-multiline-flexible"
+          label="Mail adress"
+          multiline
+          maxRows={4}
+          value={value}
+          placeholder="name@gmail.com"
+          onChange={handleChange}
+          variant="filled"
+        />
+        <TextField
+          id="outlined-textarea"
+          label="Recipe label"
+          placeholder="Placeholder"
+          multiline
+          variant="filled"
+        />
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+          defaultValue="Ingredients, image and url"
+          variant="filled"
+        />
+
+      
+      </div>
+    </Box>
+    </>
+  );
+}
