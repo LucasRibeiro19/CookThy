e851ce8223be18c728c5ccdc8cbc59de6acd9e17
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-05-18 21:43:22 +0200

    Fixed liked recipes counter

--- src/components/LikedRecipes.jsx
+++ src/components/LikedRecipes.jsx
@@ -23,7 +23,7 @@ function LikedRecipes() {
         color='#01937C'
         component="div"
         sx={{  display: {fontWeight:'bold'}, padding:"5%" }} 
-      > {recipeLike.length} recipes saved
+      > {recipeLike.length === 1 ? '1 recipe saved' :`${recipeLike.length} recipes saved` }
       </Typography>
         {recipeLike.map((likedRecipe, index) => <LikedRecipe recipeLike={likedRecipe} key={index}/>)}
       </div>
