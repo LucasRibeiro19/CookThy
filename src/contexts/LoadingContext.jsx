383ba4b77209131dcb8d4dbb3d9576a7bc3b45f2
Lucas <90116481+LucasRibeiro19@users.noreply.github.com>
2022-06-15 17:50:00 +0200

    Removed logs from LoadingContext

--- src/contexts/LoadingContext.jsx
+++ src/contexts/LoadingContext.jsx
@@ -6,8 +6,6 @@ export const LoadingContextProvider = ({children}) => {
 
     const [loading, setLoading] = useState(false);
 
-    console.log(loading);
-
     return (
         <LoadingContext.Provider
             value={{
